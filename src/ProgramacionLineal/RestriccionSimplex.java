dc1607a8bdb30d6175f7145092cad64f3d923bc7
MijelWhuu <miguel.garcia.0129@gmail.com>
2019-03-27 18:24:43 -0600

    Inicio de simplex uwu

---
+++ src/ProgramacionLineal/RestriccionSimplex.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgramacionLineal;
+
+/**
+ *
+ * @author <Mikel>
+ */
+public class RestriccionSimplex {
+    private double[] valores;
+    private char[] variable;
+
+    public RestriccionSimplex() {
+        
+    }
+    public boolean validarRestriccion(String restriccion){
+        if(restriccion.contains("-")) return false;
+        restriccion=restriccion.replace(" ", "");
+        String[] valores= restriccion.split("\\+");
+        this.valores= new double[valores.length];
+        this.variable=new char[valores.length];
+        try{
+            for(int i=0;i<this.variable.length;i++){
+                int aux= valores[i].length();
+                variable[i]=valores[i].charAt(aux-1);
+                if(aux==1){
+                    this.valores[i]=1;
+                }else{
+                    this.valores[i]= Double.parseDouble(valores[i].substring(0, aux-1));
+                }
+                if(restriccion.lastIndexOf(variable[i])!=restriccion.indexOf(variable[i]))
+                    return false;
+            }
+            return true;
+        }catch(NumberFormatException x){
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        RestriccionSimplex r = new RestriccionSimplex();
+        System.out.println(r.validarRestriccion("2.5x + x "));
+    }
+    
+}
